35a99c024d85a5da3d844b921a8b76129891465e
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2024-04-09 22:09:46 +0000

    trying to fix conditional rendering on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Cinema App',
-};
+'use client'; // Add this line at the top of your file to mark the component for client-side execution
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
+  interface UserData {
+    userId: number;
+    username: string;
+    name: string;
+    profilePicture: string;
+    isAdmin: boolean;
+  }
+
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    const userDataString = localStorage.getItem('userData'); // Retrieve user data string from localStorage
+    if (userDataString) {
+      console.log(userDataString, 'userDataString:');
+      const data = JSON.parse(userDataString);
+      console.log(data);
+      setUserData(data);
+    }
+  }, []);
+
   return (
     <div>
       <h1>Welcome to Cinema App</h1>
+      {userData && (
+        <div>
+          <p>Username: {(userData as UserData).username}</p>
+          <p>Name: {(userData as UserData).name}</p>
+          <Image
+            src={(userData as UserData).profilePicture}
+            alt="Profile"
+            style={{ width: '100px', height: '100px' }}
+          />
+          {(userData as UserData).isAdmin && <p>You have admin privileges.</p>}{' '}
+        </div>
+      )}
     </div>
   );
 }
