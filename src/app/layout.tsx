dc1548cb486def531a4d8d9f7600285f58800da6
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2024-04-09 22:43:16 +0000

    Fix image rendering issue, add signup link on login page, and refactor API service

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import { Inter } from 'next/font/google';
 import Navbar from '../components/Navbar';
 import '../styles/global.scss';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Cinema App',
+};
+
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
