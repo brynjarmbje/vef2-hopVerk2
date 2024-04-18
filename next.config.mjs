ee3e8226d7fc7ebeccac42d02f78e48b3e7c56f0
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2024-04-18 16:48:45 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'http',
         hostname: 'res.cloudinary.com',
         pathname: '**',
       },
